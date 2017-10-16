f7f69003af16cf27db8f039761e96f4f0e346333
Rashidul Esrar <resrar@cs.uchicago.edu>
2017-10-16 01:54:53 -0500

    added another plot

--- hw3/hw3.R
+++ hw3/hw3.R
@@ -62,26 +62,42 @@ merged_cpi %>%
   theme(legend.position = c(0.83, 0.1))
 
 # Import Quarterly Income Data from Bureau of Economic Analysis
-# https://www.bea.gov/iTable/index_regional.cfm
 income <- read_csv('personal_income_quarterly_bystate_2007-2017.csv', skip = 4)
-drop_cols <- c('2008:Q3', '2015:Q3')
+quarter_cols <- c('2008:Q3', '2015:Q3')
 drop_rows <- c('New England', 'Mideast', 'Great Lakes', 'Plains', 'Southeast', 
                'Southwest', 'Rocky Mountain', 'Far West', 'United States')
-personal_income <- income %>% 
-  filter(LineCode == 1, !GeoName %in% drop_rows) %>%
-  select(-c(LineCode, Description)) %>%
-  select(GeoName, '2008:Q3', '2015:Q3') %>%
-  mutate(t1 = as.integer(personal_income[['2008:Q3']]), t2 = as.integer(personal_income[['2015:Q3']])) %>%
-  mutate(pct_change = 100*(t2-t1)/t1)
+state_personal_income <- income %>%
+  filter(LineCode==1, !GeoName %in% drop_rows) %>%
+  select(GeoName, quarter_cols) %>%
+  mutate(state=as.character(GeoName),
+         t1=as.integer(state_personal_income[['2008:Q3']]), 
+         t2=as.integer(state_personal_income[['2015:Q3']]),
+         pct_change=100*(t2-t1)/t1) %>%
+  select(state, t1, t2, pct_change) %>%
+  mutate(state=factor(state, levels=unique(state)))
 
-ggplot(personal_income, aes(x=GeoName, y=pct_change)) + 
-  geom_point(stat='identity', color='blue') +
-  theme(axis.text.x=element_text(angle=90, hjust=1)) +
-  geom_smooth(method='lm', aes(group=1), color='black', linetype='dashed', se=FALSE) +
-  labs(title='Percent Change in Personal Income by State',
-       subtitle='Percent Change calculated using 2015Q3 and 2008Q3 data',
-       caption='Source: Bureau of Economic Analysis (US Dept of Commerce)', 
-       x='State', y='Percent Change')
+# Plot Graph 3
+state_personal_income %>%
+  ggplot(aes(x=state, y=pct_change)) + 
+  theme(axis.text.x=element_text(angle=90, hjust=1),
+        legend.position=c(0,0)) +
+  geom_point(color='#000000', alpha=0.65, stat='identity', size=0.75) +
+  geom_hline(yintercept=median(state_personal_income$pct_change), 
+             aes(linetype='Median Percent Change'),
+             size=0.5, alpha=0.65, linetype='dashed') +
+  geom_hline(yintercept=9.8, aes(linetype='Percent Change in CPI'),
+             size=0.5, alpha=0.85, linetype='dashed') +
+  labs(title='Changes in Personal Income Outpaced Change in Inflation',
+     subtitle='Percent Change calculated using 2015Q3 and 2008Q3 data',
+     caption='Source: Bureau of Economic Analysis (US Dept of Commerce)', 
+     x='State', y='Percent Change') + 
+  annotate("text", 'South Dakota', 25.5, label='Median Percent Change') +
+  annotate("text", 'South Dakota', 14, label='Percent Change in CPI')
+
+
+
+
+
 
 per_income <- melt(personal_income, id.var='GeoName')
 per_income <- per_income %>%
